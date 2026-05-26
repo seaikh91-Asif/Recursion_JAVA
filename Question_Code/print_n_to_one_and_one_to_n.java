3fe692cff4bcc5af55f19fa378cf6ee7d86d35d3
seaikh91-Asif <seaikh91@gmail.com>
2026-05-26 10:31:39 +0530

    printing one to n and n to one using recursion

--- Question_Code/print_n_to_one_and_one_to_n.java
+++ Question_Code/print_n_to_one_and_one_to_n.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class print_n_to_one_and_one_to_n {
     public static void main(String[] args) {
-        System.out.print("hello world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter a number: ");
+        int n = scanner.nextInt();
+//        NtoOne(n);
+//        OnetoN(n);
+        both(n);
+    }
+    static void NtoOne(int n){
+        if( n == 0){
+            return;
+        }
+        System.out.println(n);
+        NtoOne(n - 1);
+    }
+
+    static void OnetoN(int n){
+        if( n == 0){
+            return;
+        }
+        OnetoN(n - 1);
+        System.out.println(n);
+    }
+
+    static void both(int n){
+        if( n == 0){
+            return;
+        }
+        System.out.println(n);
+        both(n - 1);
+        System.out.println(n);
     }
 }
