a8732634b7bbf4fdc89e1e7dc7f6e1a37fdbccb7
seaikh91-Asif <seaikh91@gmail.com>
2026-05-27 19:04:45 +0530

    adding comment for separating functions

--- Question_Code/print_n_to_one_and_one_to_n.java
+++ Question_Code/print_n_to_one_and_one_to_n.java
@@ -9,6 +9,7 @@ public class print_n_to_one_and_one_to_n {
 //        OnetoN(n);
         both(n);
     }
+    // function for printing n to one
     static void NtoOne(int n){
         if( n == 0){
             return;
@@ -16,7 +17,7 @@ public class print_n_to_one_and_one_to_n {
         System.out.println(n);
         NtoOne(n - 1);
     }
-
+    // function for printing one to n
     static void OnetoN(int n){
         if( n == 0){
             return;
@@ -24,7 +25,7 @@ public class print_n_to_one_and_one_to_n {
         OnetoN(n - 1);
         System.out.println(n);
     }
-
+    // function for printing both one to n and n to one
     static void both(int n){
         if( n == 0){
             return;
