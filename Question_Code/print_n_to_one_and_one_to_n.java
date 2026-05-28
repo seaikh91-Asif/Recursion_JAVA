6370b1e739eadd72775f3b17c34777d3f6a1198c
seaikh91-Asif <seaikh91@gmail.com>
2026-05-28 22:32:50 +0530

    final review

--- Question_Code/print_n_to_one_and_one_to_n.java
+++ Question_Code/print_n_to_one_and_one_to_n.java
@@ -9,6 +9,7 @@ public class print_n_to_one_and_one_to_n {
           OnetoN(n);
           both(n);
     }
+
     // function for printing n to one
     static void NtoOne(int n){
         if( n == 0){
@@ -17,6 +18,7 @@ public class print_n_to_one_and_one_to_n {
         System.out.println(n);
         NtoOne(n - 1);
     }
+
     // function for printing one to n
     static void OnetoN(int n){
         if( n == 0){
@@ -25,6 +27,7 @@ public class print_n_to_one_and_one_to_n {
         OnetoN(n - 1);
         System.out.println(n);
     }
+
     // function for printing both one to n and n to one
     static void both(int n){
         if( n == 0){
