cf7e56d113a0ad1ce528015415bb91a68dcfa82a
seaikh91-Asif <seaikh91@gmail.com>
2026-05-29 10:46:34 +0530

    addition of n numbers

---
+++ Question_Code/SumOf_nNumbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOf_nNumbers {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter number: ");
+        int n = scanner.nextInt();
+        int ans = sum(n);
+        System.out.println(ans);
+
+    }
+    static int sum(int n ){
+        if(n <= 1) {
+            return 1;
+        }
+        return n + sum(n - 1);
+    }
+}
