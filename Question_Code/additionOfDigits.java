576aad42619ff17a226e07d0051f12021a072f87
seaikh91-Asif <seaikh91@gmail.com>
2026-05-29 10:59:00 +0530

    sum of digits using recursion

---
+++ Question_Code/additionOfDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class additionOfDigits {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter the digit: ");
+        int n = scanner.nextInt();
+        int ans = digits(n);
+        System.out.print(ans);
+    }
+
+    static int digits(int n ){
+        if(n == 0){
+            return 0;
+        }
+        return (n % 10) + digits(n / 10);
+    }
+}
