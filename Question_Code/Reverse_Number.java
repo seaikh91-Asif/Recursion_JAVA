a0fd1c25d454bbd618752dc6fe811b821c964535
seaikh91-Asif <seaikh91@gmail.com>
2026-05-30 15:48:10 +0530

    reverse of a number using recursion

---
+++ Question_Code/Reverse_Number.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Reverse_Number {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter the Number: ");
+        int n = scanner.nextInt();
+    }
+}
