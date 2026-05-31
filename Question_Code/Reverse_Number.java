a37156c75a90fbce25be5979b41b9f384a735c83
seaikh91-Asif <seaikh91@gmail.com>
2026-05-31 14:12:54 +0530

    reverse of a number using recursion

--- Question_Code/Reverse_Number.java
+++ Question_Code/Reverse_Number.java
@@ -1,10 +1,37 @@
 import java.util.Scanner;
 
 public class Reverse_Number {
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("enter the Number: ");
-        int n = scanner.nextInt();
+    static int sum = 0;
+    static void rev1(int n) {
+        if (n == 0) {
+            return;
+        }
+        int rem = n % 10;
+        sum = sum * 10 + rem;
+        rev1(n/10);
     }
+
+    static int rev2(int n) {
+        // sometimes you might need some additional variables in the argument
+        // in that case, make another function
+        int digits = (int)(Math.log10(n)) + 1;
+        return helper(n, digits);
+    }
+
+    private static int helper(int n, int digits) {
+        if (n%10 == n) {
+            return n;
+        }
+        int rem = n % 10;
+        return rem * (int)(Math.pow(10, digits-1)) + helper(n/10, digits-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("entaer a number: ");
+        int n = scanner.nextInt();
+        System.out.println(rev2(n));
+    }
+
 
 }
