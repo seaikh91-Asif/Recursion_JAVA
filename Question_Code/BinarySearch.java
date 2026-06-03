fdcbb9cbe48ee266402588f99bb3c9e5e5f82c2d
seaikh91-Asif <seaikh91@gmail.com>
2026-06-03 22:23:25 +0530

    binary Search using recursion

---
+++ Question_Code/BinarySearch.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+    public static void main(String[] args){
+        int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("enter what you want to find: ");
+        int target = scanner.nextInt();
+        System.out.println(search(arr, target,  0, arr.length));
+    }
+
+    static int search (int [] arr, int target, int s, int e){
+        if(s > e){
+            return -1;
+        }
+
+        int m = s + ( e - s)/2;
+
+        if (arr[m] == target){
+            return m;
+        }
+
+        if(target < arr[m]){
+           return search(arr, target,s, m - 1);
+        }
+
+        return search(arr, target, m + 1, e);
+    }
+}
