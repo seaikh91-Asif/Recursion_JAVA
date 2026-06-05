ec45e9746da39913865d57958b5696f1e026f0bb
seaikh91-Asif <seaikh91@gmail.com>
2026-06-05 21:47:17 +0530

    Cheacking that array is sorted or not using recursion

---
+++ Question_Code/SortedOrNot_UisingRecursion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SortedOrNot_UisingRecursion {
+    public static void main(String[] args){
+        int [] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(IsSorted(arr, 0));
+
+    }
+
+    static boolean IsSorted(int [] arr, int index){
+        if(arr[index] == arr.length - 1){
+            return true;
+        }
+
+        return arr[index] < arr[index + 1] && IsSorted(arr, arr[index + 1]);
+    }
+}
