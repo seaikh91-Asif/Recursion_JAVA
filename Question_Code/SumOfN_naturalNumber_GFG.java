a019ecffad747ba2987419576833f3d7f2f00d2b
seaikh91-Asif <seaikh91@gmail.com>
2026-06-06 19:26:18 +0530

    solve by self

--- Question_Code/SumOfN_naturalNumber_GFG.java
+++ Question_Code/SumOfN_naturalNumber_GFG.java
@@ -1,17 +1,14 @@
 class SumOfN_naturalNumber_GFG {
 
     public static void main(String args[]){
-        int n = 3;
+        int n = 2;
         System.out.println(recurSum(n));
     }
 
     public static int recurSum(int n){
-
-        // base case
-        if (n == 0)
+        if(n == 0){
             return 0;
-
-        // recursive case
-        return n + recurSum(n - 1);
+        }
+       return n + recurSum(n - 1);
     }
 }
