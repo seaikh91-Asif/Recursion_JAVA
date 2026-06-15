cc71f347ff731cdc1b002e85742f330e3e0f97c2
seaikh91-Asif <seaikh91@gmail.com>
2026-06-15 21:55:49 +0530

    maintaing consistency

--- Question_Code/additionOfDigits.java
+++ Question_Code/additionOfDigits.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class additionOfDigits {
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.print("enter the digit: ");
