7f65550fe986045c05d5f98717c2cc778589b5d6
seaikh91-Asif <seaikh91@gmail.com>
2026-07-01 22:03:59 +0530

    commit

--- Question_Code/Factorial.java
+++ Question_Code/Factorial.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Factorial {
-
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.print("enter a number: ");
