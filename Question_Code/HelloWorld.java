c953e65102dede47ee5efca2a541e8c4e942fdf5
seaikh91-Asif <seaikh91@gmail.com>
2026-07-05 22:52:48 +0530

    printing from one to n

--- Question_Code/HelloWorld.java
+++ Question_Code/HelloWorld.java
@@ -1,9 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args){
         Hello();
+        Print(100);
     }
 
     static void Hello(){
         System.out.println("hello world");
+    }
+
+    static void Print(int n){
+        for(int i = 0; i < n; i++){
+            System.out.println(i);
+        }
     }
 }
