728b0df369a1a2c5c7e95481093509d6e0585299
seaikh91-Asif <seaikh91@gmail.com>
2026-07-09 09:28:23 +0530

    linear search using recursion

---
+++ Question_Code/LinearSearch_Recursion.java
@@ -0,0 +1,26 @@
+public class LinearSearch_Recursion {
+    public static void main(String[] args){
+        int [] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int target = 8;
+        System.out.println(isExists(arr, target, 0));
+    }
+
+    static boolean isExists(int [] arr, int target, int index){
+       if(index == arr.length){
+           return false;
+       }
+
+       return arr[index] == target || isExists(arr, target, index +1);
+    }
+
+//    static int findIndex(int [] arr, int target, int index){
+//        if(index == arr.length){
+//            return index;
+//        }
+//        if(arr[index]  == target){
+//            return index;
+//        } else {
+//            return isExists(arr, target, index +1);
+//        }
+//    }
+}
