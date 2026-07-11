9a7a5c8b2255b974537edde7406a9a76909e1b7e
seaikh91-Asif <seaikh91@gmail.com>
2026-07-11 08:12:15 +0530

    conting the number of zeros

---
+++ Question_Code/countNumber.java
@@ -0,0 +1,23 @@
+public class countNumber {
+    public static void main(String[] args){
+        System.out.println(count(103340));
+    }
+
+    static int count(int n){
+        return helper(n, 0);
+    }
+
+    static int helper (int n, int c){
+        if(n == 0){
+            return c;
+        }
+
+        int rem = n % 10;
+
+        if(rem == 0){
+            return helper(n/10 , c+1);
+        }
+        return helper(n/10 , c);
+
+    }
+}
