04271bc060b36fbc800ab3d772a4fe0e5eff9c20
seaikh91-Asif <seaikh91@gmail.com>
2026-07-19 21:00:34 +0530

    sorting using recursion and linear search using recursion

--- Question_Code/LinearSeach_Recursion.java
+++ Question_Code/LinearSeach_Recursion.java
@@ -1,17 +1,14 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class LinearSeach_Recursion {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("enter the number that you want to seach: ");
-        int target = scanner.nextInt();
+        int [] arr = {2, 3, 4, 1, 6, 7, 8, 4, 3, 3, 4, 5, 6};
+        searchAll(arr, 4, 0);
+        System.out.print(list);
 
-        int [] arr = {1, 3, 4, 5, 6, 7, 8, 9};
-
-        System.out.println(search(arr, target, 0));
     }
     static int search(int [] arr, int target, int index){
-
         if(index == arr.length){
             return -1;
         }
@@ -19,6 +16,17 @@ public class LinearSeach_Recursion {
         if(arr[index] == target){
             return index;
         }
-        return search(arr, target, index+1);
+
+        return search(arr, target, index + 1);
+    }
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void searchAll(int [] arr, int target, int index){
+        if(index == arr.length - 1){
+            return;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+        searchAll(arr, target, index + 1);
     }
 }
