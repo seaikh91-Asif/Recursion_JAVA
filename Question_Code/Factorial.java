bf15ffe7e9b7926173493282784e9bbbf9322f5d
seaikh91-Asif <seaikh91@gmail.com>
2026-08-26 22:11:18 +0530

    sum n number

--- Question_Code/Factorial.java
+++ Question_Code/Factorial.java
@@ -11,10 +11,10 @@ public class Factorial {
     }
 
     static int factorial(int n ){
-        if(n <= 1) {
+        if(n == 1) {
             return 1;
         }
 
-        return n * factorial(n - 1);
+        return n + factorial(n - 1);
     }
 }
