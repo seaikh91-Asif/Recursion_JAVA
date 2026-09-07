96ab92bbce333909cafaefe8df4bd8aff089e1af
seaikh91-Asif <seaikh91@gmail.com>
2026-09-07 22:46:16 +0530

    commit

--- Question_Code/Factorial.java
+++ Question_Code/Factorial.java
@@ -19,12 +19,7 @@ public class Factorial {
             return 1;
         }
 
-//        commit commit commit
-
-
-
-
-
+//        commit commit commit ccommit s
 
 
         return n + factorial(n - 1);
