a18ca83e576c7c04d264198a56641162d1b8a362
seaikh91-Asif <seaikh91@gmail.com>
2026-09-15 12:25:46 +0530

    sliding window

---
+++ Question_Code/SlidingWindow.java
@@ -0,0 +1,30 @@
+public class SlidingWindow {
+    public static void main(String[] args){
+        int [] arr = { 2, 1, 5, 7, 4, 9, 6, 2, 3, 1};
+        int k = 3; // expected answer is 20
+        System.out.println(slidingWindow(arr, k));
+    }
+
+    static int slidingWindow(int arr[], int k){
+        int max = Integer.MIN_VALUE;;
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+
+        while ( j < arr.length) {
+            sum += arr[j];
+
+            if(j - i + 1 < k) {
+                j++;
+            } else if( j - i + 1 == k) {
+                max = Math.max(max, sum);
+
+                sum = sum - arr[i];
+
+                i++;
+                j++;
+            }
+        }
+        return max;
+    }
+}
