ab42343417af2f038eaa9dce2b0523c16638e56d
seaikh91-Asif <seaikh91@gmail.com>
2026-09-17 19:38:17 +0530

    commit

--- Question_Code/FristNegetiveNumber_in_window.java
+++ Question_Code/FristNegetiveNumber_in_window.java
@@ -2,6 +2,7 @@ public class FristNegetiveNumber_in_window {
     public static void main(String[] args) {
         System.out.println("hwllo world");
         System.out.println("hellow world");
+        System.out.println("hellow world");
     }
 }
 
