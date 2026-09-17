7c498b1febdb282cdc67550ae977efe70642e6fe
seaikh91-Asif <seaikh91@gmail.com>
2026-09-17 21:20:23 +0530

    commit

--- Question_Code/FristNegetiveNumber_in_window.java
+++ Question_Code/FristNegetiveNumber_in_window.java
@@ -1,9 +1,15 @@
 public class FristNegetiveNumber_in_window {
     public static void main(String[] args) {
-        System.out.println("hwllo world");
-        System.out.println("hellow world");
-        System.out.println("hellow world");
+
     }
 }
+
+/*
+  Note:
+    Challanges:
+    1) Find out Calculation
+    2)
+    3) Slid the window
+*/
 
 
